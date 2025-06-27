6eda723bbb642498c1fd045b47da5abbdad0de30
jey <ijeyleekrane@gmail.com>
2025-06-27 14:24:30 +0100

    Two text file was added

--- lib/screens/firstscreen.dart
+++ lib/screens/firstscreen.dart
@@ -11,16 +11,20 @@ class FirstScreen extends StatelessWidget {
         leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios)),
         actions: [
           IconButton(onPressed: (){}, icon: Icon(Icons.home))
+
         ],
       ),
       body: Center(
+
         child: GestureDetector(
           onTap: onTap
           ,
-          child: Container(
-            padding: EdgeInsets.all(30.0),
-            child: Text("Tapped"),
-          ),
+          child: Column(
+            children: [
+              Text("",),
+              Text(".........git............")
+            ],
+          )
         ),
       ),
     );
