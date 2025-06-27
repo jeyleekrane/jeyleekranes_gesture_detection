4e2ef22d20a75dc56223b77cb97433afc19b053e
jey <ijeyleekrane@gmail.com>
2025-06-27 13:38:49 +0100

    leading icon added to the app bar

--- lib/screens/firstscreen.dart
+++ lib/screens/firstscreen.dart
@@ -8,7 +8,7 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios)),
       ),
       body: Center(
         child: GestureDetector(
